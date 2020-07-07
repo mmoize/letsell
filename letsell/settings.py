4784746e5b93ea538292c8848c6544f8e4879c73
Moise Murhi <murhi46@gmail.com>
2020-07-07 21:18:24 +1000

    cors settings

--- letsell/settings.py
+++ letsell/settings.py
@@ -155,3 +155,18 @@ STATICFILES_DIRS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
 MEDIA_URL = '/media/'
+
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:8100',
+    'https://localhost:8100',
+    'https://101.114.183.81',
+    'https://101.114.183.81:8100',
+    'https://101.114.183.81',
+    'http://101.114.183.81',
+    'http://localhost',
+    'https://localhost',
+
+]
